2d054b043c1dd0986ee2e693c8227c1df705c9ed
rimskayama <rimskayama@outlook.com>
2023-02-11 19:23:23 +0300

    fixed validation

--- src/middlewares/blogsBodyValidationMiddleware.ts
+++ src/middlewares/blogsBodyValidationMiddleware.ts
@@ -4,26 +4,42 @@ const websiteUrlPattern =
 
 export const blogNameValidationMiddleware = body("name")
     .exists()
+    .withMessage("name is required")
     .bail()
 
-    .isLength({min: 3, max: 15})
-    .withMessage("name length should be minimum 3 and maximum 15 symbols")
+    .isString()
+    .withMessage("type of name must be string")
+    .bail()
+
+    .trim()
+    .isLength({min: 1, max: 15,})
+    .withMessage("Name length should be minimum 1 and maximum 15 symbols");
 
 
 export const blogDescriptionValidationMiddleware = body("description")
     .exists()
+    .withMessage("description is required")
     .bail()
 
-    .isLength({min: 3, max: 500})
-    .withMessage("description length should be minimum 3 and maximum 500 symbols")
+    .isString()
+    .withMessage("type of description must be string")
+    .bail()
+
+    .trim()
+    .isLength({
+        min: 1,
+        max: 500,
+    })
+    .withMessage("description length should be minimum 1 and maximum 500 symbols");
 
 export const blogWebsiteUrlValidationMiddleware = body("websiteUrl")
     .exists()
+    .withMessage('websiteURL is required')
     .bail()
 
-    .isLength({min: 3, max: 100})
-    .withMessage("websiteUrl length should be minimum 3 and maximum 100 symbols")
+    .isLength({min: 1, max: 100})
+    .withMessage("websiteUrl length should be minimum 1 and maximum 100 symbols")
     .bail()
 
     .matches(websiteUrlPattern)
-    .withMessage("Website URL must be in correct format")
+    .withMessage("website URL must be in correct format")
--- src/middlewares/postsBodyValidationMiddleware.ts
+++ src/middlewares/postsBodyValidationMiddleware.ts
@@ -2,28 +2,48 @@ import {body} from "express-validator";
 
 export const postTitleValidationMiddleware = body("title")
     .exists()
+    .withMessage('title is required')
     .bail()
 
-    .isLength({min: 0, max: 30})
-    .withMessage("name length should be maximum 30 symbols")
+    .isString()
+    .withMessage("type of title must be string")
+    .bail()
+
+    .trim()
+    .isLength({min: 1, max: 30})
+    .withMessage("name length should be minimum 1 and maximum 30 symbols")
 
 export const postDescriptionValidationMiddleware = body("shortDescription")
     .exists()
+    .withMessage('short description is required')
     .bail()
-    .isLength({min: 0, max: 100})
-    .withMessage("short description length should be maximum 100 symbols")
+
+    .isString()
+    .withMessage("type of short description must be string")
+    .bail()
+
+    .trim()
+    .isLength({min: 1, max: 100})
+    .withMessage("short description length should be minimum 1 and maximum 100 symbols")
 
 export const postContentValidationMiddleware = body("content")
     .exists()
+    .withMessage('content is required')
     .bail()
-    .isLength({min: 0, max: 1000})
-    .withMessage("content length should be maximum 100 symbols")
+
+    .isString()
+    .withMessage("type of content must be string")
+    .bail()
+
+    .trim()
+    .isLength({min: 1, max: 1000})
+    .withMessage("content length should be minimum 1 and maximum 100 symbols")
 
 export const checkBlogIdMiddleware = body("blogId")
     .exists()
-    .withMessage("blog should exist")
+    .withMessage("blog Id is required")
     .bail()
-
+    
     .isString()
     .withMessage("type of blog Id must be string")
 
