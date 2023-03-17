7815bde7f8ec319f20482868f3352dba20b1d046
rimskayama <rimskayama@outlook.com>
2023-03-17 22:45:49 +0300

    fixing

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
-import express from "express";
-import {blogsRouter} from "./routers/blogs-router";
-import {testingRouter} from "./routers/testing-router";
-import {postsRouter} from "./routers/posts-router";
 import {runDB} from "./repositories/db";
+import { app} from "./app-config";
 
-export const app = express();
-const port = process.env.PORT || 5000
-
-app.use(express.json());
-
-
-
-app.use("/blogs", blogsRouter);
-app.use("/testing", testingRouter);
-app.use("/posts", postsRouter);
-
+export const port = process.env.PORT || 5000
 const startApp = async () => {
     await runDB()
     app.listen(port, () => {
