3e2797540e33a029cf6ed7dde844cc72f4203bec
rimskayama <rimskayama@outlook.com>
2023-03-20 06:12:05 +0300

    findBlogByName

--- src/domain/posts-service.ts
+++ src/domain/posts-service.ts
@@ -1,27 +1,39 @@
 import {postModelWithMongoId} from "../models/postViewModel";
 import {postsRepository} from "../repositories/mongodb/posts-repository-mongodb";
 import {ObjectId} from "mongodb";
+import {blogsRepository} from "../repositories/mongodb/blogs-repository-mongodb";
 
 export const postsService = {
 
     async createPost(title: string, shortDescription: string,
-                     content: string, blogId: string, blogName: string) {
+                     content: string, blogId: string) {
 
-        const newPost : postModelWithMongoId = {
-            _id: new ObjectId(),
-            title: title,
-            shortDescription: shortDescription,
-            content: content,
-            blogId: blogId,
-            blogName: blogName,
-            createdAt: (new Date()).toISOString(),
-        }
+        let foundBlogByName = await blogsRepository.findBlogByName(blogId)
+
+        if (foundBlogByName) {
+            const newPost : postModelWithMongoId = {
+                _id: new ObjectId(),
+                title: title,
+                shortDescription: shortDescription,
+                content: content,
+                blogId: blogId,
+                blogName: foundBlogByName.name,
+                createdAt: (new Date()).toISOString(),
+            }
+
         return await postsRepository.createPost(newPost);
-    },
+    }
+        },
 
     async updatePost(_id: ObjectId, title: string, shortDescription: string,
                      content: string, blogId: string) {
-        return await postsRepository.updatePost(_id, title, shortDescription, content, blogId);
+
+        let foundBlogByName = await blogsRepository.findBlogByName(blogId)
+
+        if (foundBlogByName) {
+            return await postsRepository.updatePost(_id, title, shortDescription, content, blogId);
+        }
+
     },
 
     async deletePost(_id: ObjectId) {
