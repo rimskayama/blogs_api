523e8207560ba94a91605640fa17b93995c2adf6
rimskayama <rimskayama@outlook.com>
2023-03-20 06:23:24 +0300

    fixing

--- src/functions/randomNumber.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.randomNumber = void 0;
-function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min).toString();
-}
-exports.randomNumber = randomNumber;
