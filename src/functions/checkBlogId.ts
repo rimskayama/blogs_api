699bd6b68c6b8a26a8b37343772a82704f10037c
rimskayama <rimskayama@outlook.com>
2023-03-20 16:24:29 +0300

    fixed blogIdCheckMiddleware

--- src/functions/checkBlogId.ts
+++ src/functions/checkBlogId.ts
@@ -1,12 +1,11 @@
 import {body} from "express-validator";
 import {blogsQueryRepository} from "../repositories/query-repos/blogs-query-repository-mongodb";
-/*
+import {blogsRepository} from "../repositories/mongodb/blogs-repository-mongodb";
+import {ObjectId} from "mongodb";
+
 export const blogIdCheckMiddleware = body("blogId").custom(async (value) => {
-    const blogs = await blogsQueryRepository.findBlogs();
-    const result = blogs.filter((el) => el.id === value);
-    if (result.length < 1) {
+    let foundBlogByName = await blogsRepository.findBlogName(new ObjectId(value))
+    if (!foundBlogByName) {
         throw new Error("ID not found");
     }
 });
-
-*/
