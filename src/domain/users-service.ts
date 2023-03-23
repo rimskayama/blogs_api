e24e8c365d52184a5f40494019d022eda0916e69
rimskayama <rimskayama@outlook.com>
2023-03-24 00:39:49 +0300

    added usersService

---
+++ src/domain/users-service.ts
@@ -0,0 +1,42 @@
+import {userModelWithMongoId, userViewModel} from "../models/userViewModel";
+import bcrypt from "bcrypt";
+import {usersRepository} from "../repositories/mongodb/users-repository-mongodb";
+import {ObjectId} from "mongodb";
+export const usersService = {
+
+    async createUser (login: string, email: string, password: string): Promise<userViewModel> {
+        const passwordSalt = await bcrypt.genSalt(10)
+        const passwordHash = await this._generateHash(password, passwordSalt)
+
+        const newUser: userModelWithMongoId = {
+            _id: new ObjectId(),
+            userName: login,
+            email,
+            passwordHash,
+            passwordSalt,
+            createdAt: new Date()
+        }
+        return usersRepository.createUser(newUser)
+    },
+
+    async checkCredentials(loginOrEmail: string, password: string) {
+        const user = await usersRepository.findByLoginOrEmail(loginOrEmail)
+        if (!user) return false
+        const passwordHash = await this._generateHash(password, user.passwordSalt)
+        if (user.passwordHash !== passwordHash) {
+            return false
+        }
+
+        return true
+    },
+
+    async _generateHash(password: string, salt: string) {
+        const hash = await bcrypt.hash(password, salt)
+        console.log('hash' + hash)
+        return hash
+    },
+
+    async deleteUser(_id: ObjectId) {
+        return await usersRepository.deleteUser(_id);
+    },
+}
