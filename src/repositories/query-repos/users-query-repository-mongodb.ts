331c00ba6829700c110720d845a6410a0e814bd2
rimskayama <rimskayama@outlook.com>
2023-03-24 00:38:52 +0300

    added usersQueryRepository

---
+++ src/repositories/query-repos/users-query-repository-mongodb.ts
@@ -0,0 +1,53 @@
+import {ObjectId, SortDirection} from "mongodb";
+import {usersPaginationViewModel} from "../../models/paginationViewModels";
+import {usersCollection} from "../db";
+import {usersMapping} from "../../functions/mapping";
+import {userModelWithMongoId, userViewModelWithId} from "../../models/userViewModel";
+
+export const usersQueryRepository = {
+    async findUsers(
+        page: number, limit: number, sortDirection: SortDirection,
+        sortBy: string, searchNameTerm: string, skip: number) : Promise<usersPaginationViewModel>
+    {
+
+        let allUsers = await usersCollection.find(
+            {name: {$regex: searchNameTerm, $options: 'i'}},
+        )
+            .skip(skip)
+            .limit(limit)
+            .sort( {[sortBy]: sortDirection})
+            .toArray()
+
+        const total = await usersCollection.countDocuments(
+            { name: { $regex: searchNameTerm, $options: 'i' }})
+
+        const pagesCount = Math.ceil(total / limit)
+//todo fix findUsers
+        return {
+            pagesCount: pagesCount,
+            page: page,
+            pageSize: limit,
+            totalCount: total,
+            items: usersMapping(allUsers)
+        }
+    },
+
+    async findUserById (
+        _id: ObjectId): Promise<userViewModelWithId | null> {
+        const user: userModelWithMongoId | null = await usersCollection.findOne({_id});
+        if (!user) {
+            return null
+        }
+        return {
+            id: user._id.toString(),
+            userName: user.userName,
+            email: user.email,
+            passwordHash: user.passwordHash,
+            passwordSalt: user.passwordSalt,
+            createdAt: user.createdAt
+        }
+
+    },
+
+
+}
