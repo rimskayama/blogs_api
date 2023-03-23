ebc630c17267ea1e3634315e4cbf1095b221c17f
rimskayama <rimskayama@outlook.com>
2023-03-24 00:37:57 +0300

    authRouter, usersRouter

--- src/settings.ts
+++ src/settings.ts
@@ -2,6 +2,8 @@ import express from "express";
 import {blogsRouter} from "./routers/blogs-router";
 import {testingRouter} from "./routers/testing-router";
 import {postsRouter} from "./routers/posts-router";
+import {authRouter} from "./routers/auth-router";
+import {usersRouter} from "./routers/users-router";
 
 
 export const app = express();
@@ -11,3 +13,5 @@ app.use(express.json());
 app.use("/", blogsRouter);
 app.use("/", testingRouter);
 app.use("/", postsRouter);
+app.use("/", authRouter);
+app.use("/", usersRouter);
