27dc3decec19ffdab84b477ad3482cf1951cbec7
rimskayama <rimskayama@outlook.com>
2023-03-24 13:59:26 +0300

    added validation

--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -1,12 +1,24 @@
 import {Request, Response, Router} from "express"
 import {usersService} from "../domain/users-service";
+import {
+    emailValidationMiddleware,
+    loginValidationMiddleware,
+    passwordValidationMiddleware
+} from "../middlewares/usersValidationMiddleware";
+import {basicAuthMiddleware} from "../middlewares/basicAuth";
+import {errorsValidationMiddleware} from "../middlewares/errorsValidationMiddleware";
 
 export const authRouter = Router({})
 
 authRouter.post('/auth/login',
+    basicAuthMiddleware,
+    loginValidationMiddleware,
+    passwordValidationMiddleware,
+    emailValidationMiddleware,
+    errorsValidationMiddleware,
     async (req: Request, res: Response) => {
         const checkResult = await usersService.checkCredentials(req.body.loginOrEmail, req.body.password)
         if (checkResult) {
-            res.status(201)
+            res.status(204)
         } else res.status(401)
 })
