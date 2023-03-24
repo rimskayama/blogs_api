bc3aaf32d31b3bc773decdf3f203941114e556a6
rimskayama <rimskayama@outlook.com>
2023-03-24 15:43:20 +0300

    deleted validation

--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -1,18 +1,12 @@
 import {Request, Response, Router} from "express"
 import {usersService} from "../domain/users-service";
-import {loginOrEmailValidationMiddleware, passwordValidationMiddleware
-} from "../middlewares/usersValidationMiddleware";
-import {errorsValidationMiddleware} from "../middlewares/errorsValidationMiddleware";
 
 export const authRouter = Router({})
 
 authRouter.post('/auth/login',
-    loginOrEmailValidationMiddleware,
-    passwordValidationMiddleware,
-    errorsValidationMiddleware,
     async (req: Request, res: Response) => {
         const checkResult = await usersService.checkCredentials(req.body.loginOrEmail, req.body.password)
         if (checkResult) {
-            res.status(204)
-        } else res.status(401)
+            res.sendStatus(204)
+        } else res.sendStatus(401)
 })
