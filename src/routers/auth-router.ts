a97361504815beb673e6cb4f4146f37320778a7d
rimskayama <rimskayama@outlook.com>
2023-03-24 13:23:13 +0300

    fixed authRouter

--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -3,7 +3,10 @@ import {usersService} from "../domain/users-service";
 
 export const authRouter = Router({})
 
-authRouter.post('/login',
+authRouter.post('/auth/login',
     async (req: Request, res: Response) => {
-    const checkResult = await usersService.checkCredentials(req.body.loginOrEmail, req.body.password)
-    })
+        const checkResult = await usersService.checkCredentials(req.body.loginOrEmail, req.body.password)
+        if (checkResult) {
+            res.status(201)
+        } else res.status(401)
+})
