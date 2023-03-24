dea565cb08fcaf85f22fd0b6bf16e44868170ac9
rimskayama <rimskayama@outlook.com>
2023-03-24 14:40:39 +0300

    fixed validation

--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -1,19 +1,14 @@
 import {Request, Response, Router} from "express"
 import {usersService} from "../domain/users-service";
-import {
-    emailValidationMiddleware,
-    loginValidationMiddleware,
-    passwordValidationMiddleware
+import {loginOrEmailValidationMiddleware, passwordValidationMiddleware
 } from "../middlewares/usersValidationMiddleware";
-import {basicAuthMiddleware} from "../middlewares/basicAuth";
 import {errorsValidationMiddleware} from "../middlewares/errorsValidationMiddleware";
 
 export const authRouter = Router({})
 
 authRouter.post('/auth/login',
-    loginValidationMiddleware,
+    loginOrEmailValidationMiddleware,
     passwordValidationMiddleware,
-    emailValidationMiddleware,
     errorsValidationMiddleware,
     async (req: Request, res: Response) => {
         const checkResult = await usersService.checkCredentials(req.body.loginOrEmail, req.body.password)
