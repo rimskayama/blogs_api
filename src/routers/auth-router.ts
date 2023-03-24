eabfbd2656dfe0bdf9191351154772a3319b8dbb
rimskayama <rimskayama@outlook.com>
2023-03-24 14:25:37 +0300

    deleted middleware

--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -11,7 +11,6 @@ import {errorsValidationMiddleware} from "../middlewares/errorsValidationMiddlew
 export const authRouter = Router({})
 
 authRouter.post('/auth/login',
-    basicAuthMiddleware,
     loginValidationMiddleware,
     passwordValidationMiddleware,
     emailValidationMiddleware,
