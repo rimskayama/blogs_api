9e4f738703f7095dfdcfb472558666187452dbca
rimskayama <rimskayama@outlook.com>
2023-03-24 13:24:31 +0300

    fixed findByLoginOrEmail

--- src/repositories/mongodb/users-repository-mongodb.ts
+++ src/repositories/mongodb/users-repository-mongodb.ts
@@ -1,35 +1,27 @@
-import {userModelWithMongoId, userViewModelWithId} from "../../models/userViewModel";
+import {userInputModel, userModelWithMongoId, userViewModelWithId} from "../../models/userViewModel";
 import {ObjectId} from "mongodb";
 import {usersCollection} from "../db";
 
 export const usersRepository = {
 
-    async createUser (newUser : userModelWithMongoId): Promise<userViewModelWithId> {
+    async createUser (newUser : userInputModel): Promise<userViewModelWithId> {
 
         const result = await usersCollection.insertOne(newUser)
         return {
             id: newUser._id.toString(),
-            userName: newUser.userName,
+            login: newUser.login,
             email: newUser.email,
-            passwordHash: newUser.passwordHash,
-            passwordSalt: newUser.passwordSalt,
             createdAt: newUser.createdAt
         }
     },
 
-    async findByLoginOrEmail(loginOrEmail: string): Promise<userViewModelWithId | null> {
-        const user: userModelWithMongoId | null = await usersCollection.findOne({loginOrEmail});
+    async findByLoginOrEmail(loginOrEmail: string): Promise<userInputModel| null> {
+        const user: userInputModel | null = await usersCollection.findOne({loginOrEmail});
         if (!user) {
             return null
         }
-        return {
-            id: user._id.toString(),
-            userName: user.userName,
-            email: user.email,
-            passwordHash: user.passwordHash,
-            passwordSalt: user.passwordSalt,
-            createdAt: user.createdAt
-        }
+        return user
+
     },
 
     async deleteUser(_id: ObjectId) {
