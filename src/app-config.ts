83b1a9afc669c913229311f0c3c42d5dabbac396
rimskayama <rimskayama@outlook.com>
2023-03-25 12:27:13 +0300

    added app-config.ts

---
+++ src/app-config.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import {blogsRouter} from "./routers/blogs-router";
+import {testingRouter} from "./routers/testing-router";
+import {postsRouter} from "./routers/posts-router";
+import {authRouter} from "./routers/auth-router";
+import {usersRouter} from "./routers/users-router";
+import {commentsRouter} from "./routers/comments-router";
+
+
+export const app = express();
+app.use(express.json());
+
+
+app.use("/", blogsRouter);
+app.use("/", testingRouter);
+app.use("/", postsRouter);
+app.use("/", authRouter);
+app.use("/users", usersRouter);
+app.use("/comments", commentsRouter);
