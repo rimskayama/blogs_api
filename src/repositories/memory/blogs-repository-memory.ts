51682b7d18ec15eb049493e0b6b81a3f7d974563
rimskayama <rimskayama@outlook.com>
2023-03-25 12:29:53 +0300

    refactoring

--- src/repositories/memory/blogs-repository-memory.ts
+++ src/repositories/memory/blogs-repository-memory.ts
@@ -1,5 +1,5 @@
 
-import {blogViewModel, blogViewModelWithId} from "../../models/blogViewModel";
+import {blogViewModel, blogViewModelWithId} from "../../models/blog-view-model";
 import {randomNumber} from "../../functions/randomNumber";
 
 const blogs : blogViewModelWithId[] = []
