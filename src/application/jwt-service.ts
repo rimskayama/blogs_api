4d7adb130a73ddc7c5b8954c8bb394d48d3faf74
rimskayama <rimskayama@outlook.com>
2023-03-25 12:38:09 +0300

    added jwtService

---
+++ src/application/jwt-service.ts
@@ -0,0 +1,22 @@
+import {userInputModel} from "../models/user-view-model";
+import jwt from "jsonwebtoken"
+import {ObjectId} from "mongodb";
+import {settings} from "../settings";
+
+export const jwtService  = {
+    async createJWT(user: userInputModel) {
+        const token = jwt.sign({userId: user._id}, settings.JWT_SECRET, {expiresIn: '1h'})
+        return {
+            "accessToken": token
+            }
+    },
+
+    async getUserIdByToken(token: string) {
+        try {
+            const result: any = jwt.verify(token, settings.JWT_SECRET)
+            return new ObjectId(result.userId)
+        } catch(error) {
+            return null
+        }
+    }
+}
--- src/settings.ts
+++ src/settings.ts
@@ -1,17 +1,3 @@
-import express from "express";
-import {blogsRouter} from "./routers/blogs-router";
-import {testingRouter} from "./routers/testing-router";
-import {postsRouter} from "./routers/posts-router";
-import {authRouter} from "./routers/auth-router";
-import {usersRouter} from "./routers/users-router";
-
-
-export const app = express();
-app.use(express.json());
-
-
-app.use("/", blogsRouter);
-app.use("/", testingRouter);
-app.use("/", postsRouter);
-app.use("/", authRouter);
-app.use("/", usersRouter);
+export const settings = {
+    JWT_SECRET: process.env.JWT_SECRET || "123",
+};
