380fd181e563e17da0e9ffede71d36a5892e70fb
rimskayama <rimskayama@outlook.com>
2023-03-27 12:34:40 +0300

    added comment-owner-validation.ts

---
+++ src/middlewares/comment-owner-validation.ts
@@ -0,0 +1,19 @@
+import {NextFunction, Response, Request} from "express";
+import {jwtService} from "../application/jwt-service";
+import {commentsQueryRepository} from "../repositories/query-repos/comments-query-repository-mongodb";
+import {ObjectId} from "mongodb";
+
+export const commentOwnerValidation = async (req: Request, res: Response, next: NextFunction) => {
+
+    const token = req.headers.authorization!.split(' ')[1]
+
+    const getUserIdByToken = await jwtService.getUserIdByToken(token);
+
+    const getUserIdFromComment = await commentsQueryRepository.findCommentById(new ObjectId(req.params.id));
+    const userId = getUserIdFromComment?.commentatorInfo.userId;
+
+    if (getUserIdByToken!.toString() === userId) {
+        next()
+    }  else res.sendStatus(403)
+
+}
