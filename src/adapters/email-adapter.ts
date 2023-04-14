896b082659655ebd3072fadeae5c9a0e9b7788a4
rimskayama <rimskayama@outlook.com>
2023-04-14 13:15:54 +0300

    added email-adapter

---
+++ src/adapters/email-adapter.ts
@@ -0,0 +1,27 @@
+import nodemailer from "nodemailer"
+
+export const emailAdapter = {
+
+async sendEmail(email: string, subject: string, html: string) {
+
+    const authEmail = process.env.EMAIL;
+    const authPass = process.env.PASS;
+
+            let transport = await nodemailer.createTransport({
+                    service: "outlook",
+                    auth: {
+                        user: authEmail,
+                        pass: authPass
+                    }
+                });
+
+    let info = await transport.sendMail({
+        from: authEmail,
+        to: email,
+        subject: subject,
+        html: html
+    });
+    console.log(info)
+        return info
+
+}}
