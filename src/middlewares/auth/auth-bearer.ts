1c634b7f19353dab2cfcdbf201bf23dcdff8a396
rimskayama <rimskayama@outlook.com>
2023-08-31 00:26:38 +0300

    fixing

--- src/middlewares/auth/auth-bearer.ts
+++ src/middlewares/auth/auth-bearer.ts
@@ -11,7 +11,7 @@ export const authBearerMiddleware = async (req: Request, res: Response, next: Ne
 
     const token = req.headers.authorization.split(' ')[1]
 
-    const userId = await jwtService.getUserIdByToken(token)
+    const userId = await jwtService.getUserIdByAccessToken(token)
     if (!userId) {
         return res.sendStatus(401)
     }
